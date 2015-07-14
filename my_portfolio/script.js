2a41ffb40a4f07470d2755ca369c5c3f39dc23ff
Oboidhe Lydia Iruoghene <djewel4u@gmail.com>
2015-07-14 17:38:22 +0100

    Lydia's Online Portfolio

---
+++ my_portfolio/script.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+ function over1()
+{
+	  document.getElementById("circle1").style.background="#444444";
+}
+ 
+ function over2()
+ {
+	   document.getElementById("circle2").style.background="#444444";
+ }
+ 
+ function over3()
+ {
+	    document.getElementById("circle3").style.background="#444444";
+ }
+  
+function over5()
+{
+	    document.getElementById("imagee").style.width="500px";
+ }
+  
+   function out()
+  {
+	  document.getElementById("circle1").style.background="#96C";
+      document.getElementById("circle2").style.background="#990";
+	  document.getElementById("circle3").style.background="#F99";
+  }
+  
